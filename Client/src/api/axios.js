ae2e2983c7c56b6c53ddfda6b5707616534dda93
Mahendra <pmahendrababu49@gmail.com>
2026-07-24 02:28:01 +0530

    Connect frontend to production backend

--- Client/src/api/axios.js
+++ Client/src/api/axios.js
@@ -1,19 +1,11 @@
 import axios from "axios";
 
-
 const api = axios.create({
-
-    baseURL:
-        "http://localhost:5000/api",
+    baseURL: "https://worknest-bjcr.onrender.com/api",
 
     headers: {
-
-        "Content-Type":
-            "application/json"
-
+        "Content-Type": "application/json"
     }
-
 });
-
 
 export default api;
